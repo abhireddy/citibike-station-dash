47948e6a5204a84d83171ba3f423ae5e12744ff2
Abhi Reddy <abhireddy@Abhis-MBP.fios-router.home>
2018-11-15 00:12:43 -0500

    mapbox example

--- app.py
+++ app.py
@@ -1,29 +1,61 @@
 import os
 
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+import json
+import pandas as pd
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/plotly' +
+    '/datasets/master/2011_february_us_airport_traffic.csv')
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash()
 
 server = app.server
 
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
+    html.Div(
+        html.Pre(id='lasso', style={'overflowY': 'scroll', 'height': '100vh'}),
+        className="three columns"
     ),
-    html.Div(id='display-value')
-])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    html.Div(
+        className="nine columns",
+        children=dcc.Graph(
+            id='graph',
+            figure={
+                'data': [{
+                    'lat': df.lat, 'lon': df.long, 'type': 'scattermapbox'
+                }],
+                'layout': {
+                    'mapbox': {
+                        'accesstoken': (
+                            os.environ['MAPBOX_ACCESS_TOKEN']
+                        )
+                    },
+                    'margin': {
+                        'l': 0, 'r': 0, 'b': 0, 't': 0
+                    },
+                }
+            }
+        )
+    )
+], className="row")
+
+
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
+
+
+@app.callback(
+    Output('lasso', 'children'),
+    [Input('graph', 'selectedData')])
+def display_data(selectedData):
+    return json.dumps(selectedData, indent=2)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
