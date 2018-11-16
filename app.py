657b47e6a37e84933817694e0bfbc83f7188652b
Abhi Reddy <abhireddy@Abhis-MBP.fios-router.home>
2018-11-15 22:33:00 -0500

    first commit real data

--- app.py
+++ app.py
@@ -10,19 +10,28 @@ import dash_html_components as html
 import json
 import pandas as pd
 
-df = pd.read_csv(
-    'https://raw.githubusercontent.com/plotly' +
-    '/datasets/master/2011_february_us_airport_traffic.csv')
+# df = pd.read_csv(
+#     'https://raw.githubusercontent.com/plotly' +
+#     '/datasets/master/2011_february_us_airport_traffic.csv')
+
+df = pd.read_csv('csv/station-availability-by-time-of-day.csv')
+df['formatted_bike_uptime'] = df.bike_uptime.apply(lambda x: str(round(x*100,0)))
+df['formatted_dock_uptime'] = df.dock_uptime.apply(lambda x: str(round(x*100,0)))
+
+df = df[df.time_of_day == 'Peak Weekday Morning']
+
+scl = [[0, 'rgb(238,64,53)'],[1, 'rgb(123,192,67)']]
 
 app = dash.Dash()
 
 server = app.server
 
 app.layout = html.Div([
-    html.Div(
-        html.Pre(id='lasso', style={'overflowY': 'scroll', 'height': '100vh'}),
-        className="three columns"
-    ),
+    # html.Div(
+    #     html.Pre(id='lasso', style={'overflowY': 'scroll', 'height': '100vh'}),
+    #     className="three columns"
+    # ),
+    html.H1('Station Availability in the NYC Citi Bike Network'),
 
     html.Div(
         className="nine columns",
@@ -30,10 +39,22 @@ app.layout = html.Div([
             id='graph',
             figure={
                 'data': [{
-                    'lat': df.lat, 'lon': df.long, 'type': 'scattermapbox'
+                    'lat': df.latitude, 'lon': df.longitude,
+                    'marker': {
+                        'color': df.bike_uptime,
+                        'colorscale': scl,
+                    },
+                    'text': df.stationName + "<br />" + df.formatted_bike_uptime + "% bike uptime",
+                    'hoverinfo': 'text',
+                    'type': 'scattermapbox',
                 }],
                 'layout': {
                     'mapbox': {
+                      "center": {
+                            "lat": 40.68382604,
+                            "lon": -73.97632328
+                        },
+                        "zoom": 11,
                         'accesstoken': (
                             os.environ['MAPBOX_ACCESS_TOKEN']
                         )
@@ -53,11 +74,11 @@ app.css.append_css({
 })
 
 
-@app.callback(
-    Output('lasso', 'children'),
-    [Input('graph', 'selectedData')])
-def display_data(selectedData):
-    return json.dumps(selectedData, indent=2)
+# @app.callback(
+#     Output('lasso', 'children'),
+#     [Input('graph', 'selectedData')])
+# def display_data(selectedData):
+#     return json.dumps(selectedData, indent=2)
 
 
 if __name__ == '__main__':
